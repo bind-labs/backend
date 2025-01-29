d4f3c3ac41ddd6004e0729e4825aaa9d9745f6ec
Liam Dyer <liamcdyer@gmail.com>
2025-01-29 16:01:31 -0500

    feat: working setup migration

--- migrations/1_setup.sql
+++ migrations/1_setup.sql
@@ -8,13 +8,15 @@ CREATE TYPE icon AS (icon text, hex_color text);
 ---- Feeds -----
 ----------------
 
+CREATE TYPE feed_type AS ENUM ('rss', 'atom', 'json');
 CREATE TABLE feed (
   id SERIAL PRIMARY KEY,
+  type feed_type NOT NULL,
   link text NOT NULL UNIQUE,
   domain text NOT NULL,
 
   title text NOT NULL,
-  description text,
+  description text NOT NULL,
   icon text,
   language char(2) NOT NULL DEFAULT 'en', -- ISO 639-1 code
 
@@ -25,9 +27,9 @@ CREATE TABLE feed (
 
   created_at timestamptz NOT NULL DEFAULT NOW(),
   updated_at timestamptz NOT NULL DEFAULT NOW()
-)
+);
 CREATE INDEX feed_link ON feed (link);
-CREATE INDEX feed_last_updated ON feed (last_updated);
+CREATE INDEX feed_updated_at ON feed (updated_at);
 CREATE INDEX feed_suspended ON feed (suspended);
 
 ----------------
@@ -41,7 +43,7 @@ CREATE TYPE feed_item_enclosure AS (
 );
 CREATE TABLE feed_item (
   id SERIAL PRIMARY KEY,
-  feed_id integer NOT NULL REFERENCES feed (id),
+  feed_id integer NOT NULL REFERENCES feed (id) ON DELETE CASCADE,
   guid text, -- Globally unique identifier, defined by the feed
   index_in_feed integer NOT NULL UNIQUE, -- Index of the item in the feed, where larger numbers are more recent
 
@@ -55,21 +57,39 @@ CREATE TABLE feed_item (
   published_at timestamptz,
 
   created_at timestamptz NOT NULL DEFAULT NOW(),
-  updated_at timestamptz NOT NULL DEFAULT NOW(),
+  updated_at timestamptz NOT NULL DEFAULT NOW()
 );
-ALTER TABLE feed_item ADD CONSTRAINT feed_item_feed_id_fkey FOREIGN KEY (feed_id) REFERENCES feed (id) ON DELETE CASCADE;
 CREATE INDEX feed_item_feed_id ON feed_item (feed_id);
 CREATE INDEX feed_item_link ON feed_item (link);
 CREATE INDEX feed_item_index_in_feed ON feed_item (index_in_feed DESC);
 
 CREATE TABLE feed_item_parsed (
   id SERIAL PRIMARY KEY,
-  feed_item_id integer NOT NULL REFERENCES feed_item (id),
+  feed_item_id integer NOT NULL REFERENCES feed_item (id) ON DELETE CASCADE,
   content text NOT NULL,
   created_at timestamptz NOT NULL DEFAULT NOW(),
   updated_at timestamptz NOT NULL DEFAULT NOW()
 );
-ALTER TABLE feed_item_parsed ADD CONSTRAINT feed_item_parsed_feed_item_id_fkey FOREIGN KEY (feed_item_id) REFERENCES feed_item (id) ON DELETE CASCADE;
+CREATE INDEX feed_item_parsed_feed_item_id ON feed_item_parsed (feed_item_id);
+
+----------------
+----- User -----
+----------------
+
+CREATE TYPE auth_provider AS ENUM ('google', 'github', 'apple');
+CREATE TABLE "user" (
+  id SERIAL PRIMARY KEY,
+
+  email text NOT NULL UNIQUE,
+  username text NOT NULL UNIQUE,
+  providers auth_provider[] NOT NULL DEFAULT '{}',
+  password_hash text,
+  passwordless_pub_key text,
+  refresh_tokens text[],
+
+  created_at timestamptz NOT NULL DEFAULT NOW(),
+  updated_at timestamptz NOT NULL DEFAULT NOW()
+);
 
 ----------------
 ---- Index -----
@@ -77,7 +97,7 @@ ALTER TABLE feed_item_parsed ADD CONSTRAINT feed_item_parsed_feed_item_id_fkey F
 
 CREATE TABLE user_index (
   id SERIAL PRIMARY KEY,
-  owner integer NOT NULL REFERENCES user (id),
+  owner integer NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
 
   query text NOT NULL,
   sort text NOT NULL,
@@ -89,12 +109,13 @@ CREATE TABLE user_index (
   created_at timestamptz NOT NULL DEFAULT NOW(),
   updated_at timestamptz NOT NULL DEFAULT NOW()
 );
-ALTER TABLE user_index ADD CONSTRAINT user_index_owner_fkey FOREIGN KEY (owner) REFERENCES user (id) ON DELETE CASCADE;
 CREATE INDEX user_index_owner ON user_index (owner);
 
+CREATE TYPE tag_child_type AS ENUM ('feed', 'index');
+CREATE TYPE tag_child AS (type tag_child_type, id integer);
 CREATE TABLE user_tag (
   id SERIAL PRIMARY KEY,
-  owner integer NOT NULL REFERENCES user (id),
+  owner integer NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
 
   title text NOT NULL,
   children tag_child[] NOT NULL DEFAULT '{}',
@@ -102,15 +123,12 @@ CREATE TABLE user_tag (
   created_at timestamptz NOT NULL DEFAULT NOW(),
   updated_at timestamptz NOT NULL DEFAULT NOW()
 );
-ALTER TABLE user_tag ADD CONSTRAINT user_tag_owner_fkey FOREIGN KEY (owner) REFERENCES user (id) ON DELETE CASCADE;
-CREATE TYPE tag_child AS (type tag_child_type, id integer);
-CREATE TYPE tag_child_type AS ENUM ('feed', 'index');
 CREATE INDEX user_tag_owner ON user_tag (owner);
 
 CREATE TABLE user_feed (
   id SERIAL PRIMARY KEY,
-  owner integer NOT NULL REFERENCES user (id),
-  feed integer NOT NULL REFERENCES feed (id),
+  owner integer NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
+  feed integer NOT NULL REFERENCES feed (id) ON DELETE CASCADE,
 
   title text NOT NULL,
   icon icon,
@@ -118,8 +136,6 @@ CREATE TABLE user_feed (
   created_at timestamptz NOT NULL DEFAULT NOW(),
   updated_at timestamptz NOT NULL DEFAULT NOW()
 );
-ALTER TABLE user_feed ADD CONSTRAINT user_feed_owner_fkey FOREIGN KEY (owner) REFERENCES user (id) ON DELETE CASCADE;
-ALTER TABLE user_feed ADD CONSTRAINT user_feed_feed_fkey FOREIGN KEY (feed) REFERENCES feed (id) ON DELETE CASCADE;
 
 ----------------
 ----- List -----
@@ -127,7 +143,7 @@ ALTER TABLE user_feed ADD CONSTRAINT user_feed_feed_fkey FOREIGN KEY (feed) REFE
 
 CREATE TABLE user_list (
   id SERIAL PRIMARY KEY,
-  owner integer NOT NULL REFERENCES user (id),
+  owner integer NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
   title text NOT NULL,
   description text,
   icon icon,
@@ -135,22 +151,18 @@ CREATE TABLE user_list (
   created_at timestamptz NOT NULL DEFAULT NOW(),
   updated_at timestamptz NOT NULL DEFAULT NOW()
 );
-ALTER TABLE user_list ADD CONSTRAINT user_list_owner_fkey FOREIGN KEY (owner) REFERENCES user (id) ON DELETE CASCADE;
 CREATE INDEX user_list_owner ON user_list (owner);
 
 CREATE TABLE user_list_item (
   id SERIAL PRIMARY KEY,
   index integer NOT NULL,
-  owner integer NOT NULL REFERENCES user (id),
-  list integer NOT NULL REFERENCES user_list (id),
-  item integer NOT NULL REFERENCES feed_item (id),
+  owner integer NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
+  list integer NOT NULL REFERENCES user_list (id) ON DELETE CASCADE,
+  item integer NOT NULL REFERENCES feed_item (id) ON DELETE CASCADE,
 
   created_at timestamptz NOT NULL DEFAULT NOW(),
   updated_at timestamptz NOT NULL DEFAULT NOW()
 );
-ALTER TABLE user_list_item ADD CONSTRAINT user_list_item_owner_fkey FOREIGN KEY (owner) REFERENCES user (id) ON DELETE CASCADE;
-ALTER TABLE user_list_item ADD CONSTRAINT user_list_item_list_fkey FOREIGN KEY (list) REFERENCES user_list (id) ON DELETE CASCADE;
-ALTER TABLE user_list_item ADD CONSTRAINT user_list_item_item_fkey FOREIGN KEY (item) REFERENCES feed_item (id) ON DELETE CASCADE;
 CREATE INDEX user_list_item_index ON user_list_item (index);
 CREATE INDEX user_list_item_owner ON user_list_item (owner);
 CREATE INDEX user_list_item_list ON user_list_item (list);
@@ -158,35 +170,15 @@ CREATE INDEX user_list_item_item ON user_list_item (item);
 
 CREATE TABLE user_history_item (
   id SERIAL PRIMARY KEY,
-  owner integer NOT NULL REFERENCES user (id),
-  item integer NOT NULL REFERENCES feed_item (id),
-  progress integer NOT NULL DEFAULT 0, -- Progress in the item, 0 - 100
+  owner integer NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
+  item integer NOT NULL REFERENCES feed_item (id) ON DELETE CASCADE,
+  progress float NOT NULL DEFAULT 0, -- Progress in the item, 0 - 100
 
   created_at timestamptz NOT NULL DEFAULT NOW(),
   updated_at timestamptz NOT NULL DEFAULT NOW()
 );
-ALTER TABLE user_history_item ADD CONSTRAINT user_history_item_owner_fkey FOREIGN KEY (owner) REFERENCES user (id) ON DELETE CASCADE;
-ALTER TABLE user_history_item ADD CONSTRAINT user_history_item_item_fkey FOREIGN KEY (item) REFERENCES feed_item (id) ON DELETE CASCADE;
 CREATE INDEX user_history_item_owner ON user_history_item (owner);
 CREATE INDEX user_history_item_item ON user_history_item (item);
 CREATE INDEX user_history_item_progress ON user_history_item (progress);
 CREATE INDEX user_history_item_updated_at ON user_history_item (updated_at);
 
-----------------
------ User -----
-----------------
-
-CREATE TABLE user (
-  id SERIAL PRIMARY KEY,
-
-  email text NOT NULL UNIQUE,
-  username text NOT NULL UNIQUE,
-  providers provider[] NOT NULL DEFAULT '{}',
-  password_hash text,
-  passwordless_pub_key text,
-  refresh_tokens text[],
-
-  created_at timestamptz NOT NULL DEFAULT NOW(),
-  updated_at timestamptz NOT NULL DEFAULT NOW()
-);
-CREATE TYPE provider AS ENUM ('google', 'github', 'apple');
