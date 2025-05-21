aeac9469426a6c47af80b1d987d754380d0cb894
Aaditya Sahay <aaditya@speechify.com>
2025-05-21 17:32:40 +0530

    allow generating json openapi specs

--- src/gen_openapi.rs
+++ src/gen_openapi.rs
@@ -1,5 +1,17 @@
 use api::http::router;
-
+use std::env;
 fn main() {
-    std::fs::write("./openapi.yaml", router().to_openapi().to_yaml().unwrap()).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let mut json = false;
+    for arg in &args {
+        if arg == "--json" {
+            json = true;
+            break;
+        }
+    }
+    if json {
+        std::fs::write("./openapi.json", router().to_openapi().to_json().unwrap()).unwrap();
+    } else {
+        std::fs::write("./openapi.yaml", router().to_openapi().to_yaml().unwrap()).unwrap();
+    }
 }
